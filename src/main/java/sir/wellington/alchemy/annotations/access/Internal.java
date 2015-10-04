41e80fe5fb18d340f9606d030132b37c2ba6b18b
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-10-04 19:19:20 +0200

    Minor Tweaks
    + Making @Internal applicable to method definitions as well

--- src/main/java/sir/wellington/alchemy/annotations/access/Internal.java
+++ src/main/java/sir/wellington/alchemy/annotations/access/Internal.java
@@ -20,6 +20,7 @@ package sir.wellington.alchemy.annotations.access;
 import java.lang.annotation.Documented;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import java.lang.annotation.Target;
@@ -31,7 +32,7 @@ import java.lang.annotation.Target;
  * @author SirWellington
  */
 @Documented
-@Target({TYPE, PARAMETER, FIELD, LOCAL_VARIABLE})
+@Target({TYPE, PARAMETER, FIELD, LOCAL_VARIABLE, METHOD})
 public @interface Internal 
 {
 
