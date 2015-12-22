9ad2b631cc585884a10fbef4073050d0e7b298c8
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-22 12:25:33 -0800

    Adding @Pojo Annotation type

---
+++ src/main/java/tech/sirwellington/alchemy/annotations/objects/Pojo.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.sirwellington.alchemy.annotations.objects;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+
+
+/**
+ * Describes an Object which is designed only to hold Objects.
+ * In some circles this is known as a Java Bean.
+ * The idea is that only {@code getters/setters/toString()/equals()/hashCode()} are provided.
+ * @author SirWellington
+ */
+@Documented
+@Retention(CLASS)
+@Target(TYPE)
+public @interface Pojo 
+{
+
+}
