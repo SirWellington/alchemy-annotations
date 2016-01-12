be47e3ab701d856982560a59c9e390899b85db2d
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-11 21:45:39 -0800

    Updating @Optional annotation

--- src/main/java/tech/sirwellington/alchemy/annotations/arguments/Optional.java
+++ src/main/java/tech/sirwellington/alchemy/annotations/arguments/Optional.java
@@ -22,6 +22,7 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
@@ -31,7 +32,7 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
  * @author SirWellington
  */
 @Documented
-@Target({ PARAMETER, FIELD, LOCAL_VARIABLE })
+@Target({ PARAMETER, FIELD, LOCAL_VARIABLE, METHOD })
 @Retention(CLASS)
 public @interface Optional 
 {
