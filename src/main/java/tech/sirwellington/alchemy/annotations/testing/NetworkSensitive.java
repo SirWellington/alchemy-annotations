2487420395d87d3f553f3eb06cbd30cd938daa4d
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-29 12:29:24 -0800

    Adding @NetworkSensitive annotation

---
+++ src/main/java/tech/sirwellington/alchemy/annotations/testing/NetworkSensitive.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.sirwellington.alchemy.annotations.testing;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+
+/**
+ * Notes that an Operation is sensitive to the Network. This could mean the availability of the network,
+ * or of services on the Network.
+ * 
+ * @author SirWellington
+ */
+@Documented
+@Retention(CLASS)
+@Target({ TYPE, FIELD, METHOD })
+public @interface NetworkSensitive 
+{
+
+}
