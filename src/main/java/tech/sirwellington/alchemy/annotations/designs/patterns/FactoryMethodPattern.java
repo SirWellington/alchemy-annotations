df47859b2c0e660d3e73bc2888cf1bd968baa04b
Wellington Moreno <wellington@censio.love>
2018-01-10 22:01:16 -0800

    Renames annotation to `@FactoryMethodPattern`

--- src/main/java/tech/sirwellington/alchemy/annotations/designs/patterns/FactoryMethodPattern.java
+++ src/main/java/tech/sirwellington/alchemy/annotations/designs/patterns/FactoryMethodPattern.java
@@ -32,7 +32,7 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
 @Documented
 @Retention(CLASS)
 @Target({TYPE, METHOD, FIELD})
-public @interface FactoryMethod
+public @interface FactoryMethodPattern
 {
 
     Role role();
