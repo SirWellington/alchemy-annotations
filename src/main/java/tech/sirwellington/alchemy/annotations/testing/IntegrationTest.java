298290b6a8bfa13676d79db3f7a772126423c65e
Wellington Moreno <wellington@censio.love>
2018-06-26 05:36:34 -0700

    Updates copyrights

--- src/main/java/tech/sirwellington/alchemy/annotations/testing/IntegrationTest.java
+++ src/main/java/tech/sirwellington/alchemy/annotations/testing/IntegrationTest.java
@@ -1,11 +1,10 @@
 /*
  * Copyright © 2018. Sir Wellington.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  *
  * You may obtain a copy of the License at
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,13 +16,9 @@
 
 package tech.sirwellington.alchemy.annotations.testing;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
 
