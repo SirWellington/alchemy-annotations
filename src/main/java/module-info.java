d46d54f52a72b7f35056bc8920f8d1d3c8ab23c8
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-06-13 10:30:46 -0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,12 @@
 module tech.sirwellington.alchemy.annotations {
     requires java.base;
+    exports tech.sirwellington.alchemy.annotations.access;
+    exports tech.sirwellington.alchemy.annotations.arguments;
+    exports tech.sirwellington.alchemy.annotations.concurrency;
+    exports tech.sirwellington.alchemy.annotations.designs;
+    exports tech.sirwellington.alchemy.annotations.objects;
+    exports tech.sirwellington.alchemy.annotations.operations;
+    exports tech.sirwellington.alchemy.annotations.testing;
     opens tech.sirwellington.alchemy.annotations.access;
     opens tech.sirwellington.alchemy.annotations.arguments;
     opens tech.sirwellington.alchemy.annotations.concurrency;
